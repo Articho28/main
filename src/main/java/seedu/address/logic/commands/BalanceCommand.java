a6314c1fe4634af6eb1370ffd6f38fcde8818868
Articho28 <artsiom.sk@gmail.com>
2018-03-19 11:35:20 +0800

    Balance command now works

---
+++ src/main/java/seedu/address/logic/commands/BalanceCommand.java
@@ -0,0 +1,31 @@
+package seedu.address.logic.commands;
+
+import java.util.List;
+
+import seedu.address.model.person.Person;
+
+public class BalanceCommand extends Command {
+
+    public static final String COMMAND_WORD = "balance";
+    public static final String COMMAND_SHORTCUT = "b";
+
+    public static final String MESSAGE_SUCCESS = "Shown balance.";
+
+    public static double calculatedBalance;
+
+    @Override
+    public CommandResult execute() {
+
+        double accumulator = 0.0;
+
+        List<Person> lastShownList = model.getFilteredPersonList();
+
+
+        for (Person person : lastShownList) {
+            double currentPersonBalance = person.getMoney().balance;
+            accumulator = accumulator + currentPersonBalance;
+        }
+        calculatedBalance = accumulator;
+        return new CommandResult("Your balance is " + Double.toString(calculatedBalance) + ".");
+    }
+}
