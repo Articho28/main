327858f278a2846a6cb262052b23674a880d1120
Articho28 <artsiom.sk@gmail.com>
2018-03-19 18:41:56 +0800

    Added test for Balance Comand

--- src/main/java/seedu/address/logic/commands/BalanceCommand.java
+++ src/main/java/seedu/address/logic/commands/BalanceCommand.java
@@ -8,7 +8,7 @@ public class BalanceCommand extends Command {
 
     public static final String COMMAND_WORD = "balance";
     public static final String COMMAND_SHORTCUT = "b";
-    private static DecimalFormat FORMAT_TWO_DECIMAL_PLACES = new DecimalFormat("0.00");
+    public static DecimalFormat FORMAT_TWO_DECIMAL_PLACES = new DecimalFormat("0.00");
 
     public static final String MESSAGE_SUCCESS = "Shown balance.";
 
@@ -17,18 +17,20 @@ public class BalanceCommand extends Command {
     @Override
     public CommandResult execute() {
 
+        calculatedBalance = getBalanceFromTravelBanker();
+        return new CommandResult(MESSAGE_SUCCESS + "\n" + "Your balance is " + FORMAT_TWO_DECIMAL_PLACES.format(calculatedBalance) + ".");
+    }
+
+    public double getBalanceFromTravelBanker() {
         double accumulator = 0.00;
 
         List<Person> lastShownList = model.getFilteredPersonList();
-
 
         for (Person person : lastShownList) {
             double currentPersonBalance = person.getMoney().balance;
             accumulator = accumulator + currentPersonBalance;
         }
-        calculatedBalance = accumulator;
 
-
-        return new CommandResult(MESSAGE_SUCCESS + "\n" + "Your balance is " + FORMAT_TWO_DECIMAL_PLACES.format(calculatedBalance) + ".");
+        return accumulator;
     }
 }
--- src/test/java/seedu/address/logic/commands/BalanceCommandTest.java
+++ src/test/java/seedu/address/logic/commands/BalanceCommandTest.java
@@ -1,6 +1,42 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.Before;
+import org.junit.Test;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.UndoRedoStack;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
 public class BalanceCommandTest {
+
+    private Model model;
+    private Model expectedModel;
+    private BalanceCommand balanceCommand;
+
+
+
+    @Before
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        balanceCommand = new BalanceCommand();
+        balanceCommand.setData(model, new CommandHistory(), new UndoRedoStack());
+    }
+
+    @Test
+    public void executes_getsOverallBalanceSuccess() {
+        double balance = balanceCommand.getBalanceFromTravelBanker();
+        assertCommandSuccess(balanceCommand, model,  BalanceCommand.MESSAGE_SUCCESS
+                + "\n" + "Your balance is "
+                + BalanceCommand.FORMAT_TWO_DECIMAL_PLACES.format(balance)
+                + ".", expectedModel);
+    }
+
+
 
 
 }
