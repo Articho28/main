9ba21b4f465fcc0e65626b55b48133fce8102e91
Articho28 <artsiom.sk@gmail.com>
2018-03-28 19:39:59 +0800

    Map Command Tests Pass

--- src/test/java/seedu/address/logic/commands/MapCommandTest.java
+++ src/test/java/seedu/address/logic/commands/MapCommandTest.java
@@ -1,7 +1,12 @@
 package seedu.address.logic.commands;
 
+import static guitests.guihandles.WebViewUtil.waitUntilBrowserLoaded;
+import static junit.framework.TestCase.assertEquals;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import guitests.guihandles.BrowserPanelHandle;
 import org.junit.Before;
@@ -12,15 +17,18 @@ import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.ui.BrowserPanel;
+import seedu.address.ui.GuiUnitTest;
 
 
-public class MapCommandTest {
+public class MapCommandTest extends GuiUnitTest {
 
-    public MapCommand mapCommand;
-    private Model model;
-    private Model expectedModel;
     public BrowserPanel browserPanel;
     public BrowserPanelHandle browserPanelHandle;
+    public MapCommand mapCommand;
+
+    private Model model;
+    private Model expectedModel;
+
 
     @Before
     public void setUp() {
@@ -29,12 +37,23 @@ public class MapCommandTest {
 
         mapCommand = new MapCommand();
         mapCommand.setData(model, new CommandHistory(), new UndoRedoStack());
-        
+
+        guiRobot.interact(() -> browserPanel = new BrowserPanel());
+        uiPartRule.setUiPart(browserPanel);
+
+        browserPanelHandle = new BrowserPanelHandle(browserPanel.getRoot());
     }
 
     @Test
     public void executes_mapCommandRecognized() {
         assertCommandSuccess(mapCommand, model, MapCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test public void executes_displaysMap() throws MalformedURLException {
+        CommandResult commandResult = mapCommand.execute();
+
+        waitUntilBrowserLoaded(browserPanelHandle);
+        assertEquals(new URL(BrowserPanel.ATM_SEARCH_PAGE_URL), browserPanelHandle.getLoadedUrl());
     }
 
 
