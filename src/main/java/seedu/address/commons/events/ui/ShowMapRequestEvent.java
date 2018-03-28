aa65ea49eaf5c613bbd2b37c2cefb1d137840ac6
Articho28 <artsiom.sk@gmail.com>
2018-03-28 20:00:10 +0800

    Added javadoc

--- src/main/java/seedu/address/commons/events/ui/ShowMapRequestEvent.java
+++ src/main/java/seedu/address/commons/events/ui/ShowMapRequestEvent.java
@@ -2,6 +2,10 @@ package seedu.address.commons.events.ui;
 
 import seedu.address.commons.events.BaseEvent;
 
+
+/**
+ * An event requesting to view a map showing the nearest ATM.
+ */
 public class ShowMapRequestEvent extends BaseEvent {
 
     public String toString() {
