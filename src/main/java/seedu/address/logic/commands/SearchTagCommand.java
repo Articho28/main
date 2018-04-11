7345c58275fc46ac56fb6399fda5a4d3ae55ea0a
Articho28 <artsiom.sk@gmail.com>
2018-04-11 11:23:11 +0800

    Search Tag Command works

--- src/main/java/seedu/address/logic/commands/SearchTagCommand.java
+++ src/main/java/seedu/address/logic/commands/SearchTagCommand.java
@@ -4,8 +4,8 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.List;
 import java.util.Set;
+import java.util.function.Predicate;
 
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.person.Person;
 import seedu.address.model.tag.Tag;
 
@@ -18,37 +18,47 @@ public class SearchTagCommand extends UndoableCommand {
             + "Example: " + COMMAND_WORD
             + PREFIX_TAG + "owesMoney "
             + PREFIX_TAG + "friends";
+    public static final String MESSAGE_SUCCESS = "Found Persons with tags: ";
 
 
     private final Set<Tag> tagsToFind;
-
+    private List<Person> personsWithSpecifiedTags;
     private boolean changed = false;
 
     /**
-     * This returns a ColorTagCommand that is ready to be executed.
-     *
+     * This returns a SearchTagCommand that is ready to be executed.
      * @param tags  that need to be colored
      */
     public SearchTagCommand(Set<Tag> tags) {
         this.tagsToFind = tags;
     }
-    //Method attempts to change the color of all tags to a specified color.
     @Override
-    public CommandResult executeUndoableCommand() throws CommandException {
-        List<Person> lastShownList = model.getFilteredPersonList();
+    public CommandResult executeUndoableCommand() {
+
+      /* List<Person> lastShownList = model.getFilteredPersonList();
         for (Person person : lastShownList) {
            for (Tag tag : person.getTags()) {
                if (tagsToFind.contains(tag)) {
                    changed = true;
+                   System.out.println(person.getName());
                }
            }
-        }
-        if (changed) {
-            return new CommandResult("Tags Changed.");
+        }*/
+
+        model.updateFilteredPersonList(personHasTags());
+        if (model.getFilteredPersonList().size() > 0) {
+            return new CommandResult(MESSAGE_SUCCESS
+                    + "\n"
+                    + tagsToFind.toString()
+                    + "\n"
+                    + getMessageForPersonListShownSummary(model.getFilteredPersonList().size()));
         } else {
-            changed = false;
-            return new CommandResult("No tags found");
+            return new CommandResult("No results found.");
         }
+    }
+
+    public Predicate<Person> personHasTags() {
+        return person -> person.getTags().containsAll(tagsToFind);
     }
 
 }
